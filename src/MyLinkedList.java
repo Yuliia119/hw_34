056a7e09d4a42112d2e1f8b30159b6aa42683ea4
Yuliia <uliagrinenko1981@gmail.com>
2025-02-17 22:09:45 +0100

    add MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,2 +1,38 @@
-public class MyLinkedList {
+public class MyLinkedList implements MyList {
+    private Node head = null;
+    private Node tail = null;
+    private int size = 0;
+
+    @Override
+    public void add(Person element) { // метод добавляет элемент в конец списка
+        Node node = new Node(null, element, null); //создает новый Node
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.setNext(node);
+            node.setPrev(tail);
+            tail = node;
+        }
+        size++;
+    }
+
+    @Override
+    public int size() { //вернет количество элементов в списке
+        return size;
+    }
+
+    @Override
+    public Person get(int index) { //получит элемент по индексу
+        return null;
+    }
+
+    @Override
+    public void print() { //вывод всех элементов списка
+        Node node = head;
+        while (node != null) {
+            System.out.println(node.getValue());
+            node = node.getNext();
+        }
+    }
 }
