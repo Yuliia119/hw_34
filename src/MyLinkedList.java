f9e6758619b0449d9f59fc98b2ee1f3c8e81fb84
Yuliia <uliagrinenko1981@gmail.com>
2025-02-18 21:15:59 +0100

    add Node in MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -35,4 +35,42 @@ public class MyLinkedList implements MyList {
             node = node.getNext();
         }
     }
+
+    private static class Node {
+        private Node prev;
+        private Person value;
+        private Node next;
+
+        public Node(Node prev, Person value, Node next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+
+        public Person getValue() {
+            return value;
+        }
+
+        public void setValue(Person value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+    }
 }
+
+
